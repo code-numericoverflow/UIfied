9693489cd9056db0f9494ec078854d3b79cb260b
code-numericoverflow <30389834+code-numericoverflow@users.noreply.github.com>
2019-12-26 16:20:11 +0100

    Add simple test

---
+++ UIfied.Test/SimpleTest.ps1
@@ -0,0 +1,26 @@
+Import-Module .\UIfied
+
+$sample = {
+    $wsb = {
+        Get-Window -Caption "Title" -Components {
+            Get-StackPannel -Orientation Vertical -Components {
+                Get-Label    -Caption "Hello"
+                Get-Button   -Caption "Button" -Action {
+                    param($this)
+                    $this.Control.Caption = Get-Date
+                }
+            }
+        }
+    }
+    $h = Get-Host
+    $h.ShowFrame($wsb)
+}
+
+Set-UIWpf
+Invoke-Command -ScriptBlock $sample
+
+Set-UICF
+Invoke-Command -ScriptBlock $sample
+
+Set-UIOoui
+Invoke-Command -ScriptBlock $sample
